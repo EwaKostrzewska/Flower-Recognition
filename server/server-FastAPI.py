19411a52bdef7f4d29ec3b11c120db73973d6b7d
Ewa Kostrzewska <ewakostrzewska25@gmail.com>
2022-09-14 20:08:47 +0200

    Added CORS

--- server/server-FastAPI.py
+++ server/server-FastAPI.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, File, UploadFile 
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn 
 import util 
 from io import BytesIO
 
 app = FastAPI() 
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post('/predict')
 async def predict(file: UploadFile = File(...)):
